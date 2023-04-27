d6d859b1409f9b32c9bbae2642bda5fcb130f3b0
Victor Bo <10667379@qq.com>
2023-04-27 15:39:17 +0800

    chore: update eslint svelte rules

--- packages/eslint-config-svelte/index.js
+++ packages/eslint-config-svelte/index.js
@@ -31,14 +31,21 @@ module.exports = !SVELTE
       TS ? '@vtrbo/eslint-config-ts' : '@vtrbo/eslint-config-basic',
     ],
     rules: {
-      'svelte/html-closing-bracket-spacing': [
-        'error',
-        {
-          startTag: 'never',
-          endTag: 'never',
-          selfClosingTag: 'always',
-        },
-      ],
+      'svelte/max-attributes-per-line': 'off',
+      'svelte/no-at-html-tags': 'off',
+      'import/no-mutable-exports': 'off',
+
+      'svelte/no-dupe-on-directives': 'error',
+      'svelte/no-dupe-use-directives': 'error',
+
+      'svelte/no-export-load-in-svelte-module-in-kit-pages': 'error',
+      'svelte/no-reactive-reassign': 'off',
+      'svelte/no-store-async': 'error',
+      'svelte/valid-prop-names-in-kit-pages': 'error',
+      'svelte/no-target-blank': 'off',
+      'svelte/no-useless-mustaches': 'error',
+      'svelte/derived-has-same-inputs-outputs': 'error',
+      'svelte/html-closing-bracket-spacing': 'error',
       'svelte/html-quotes': [
         'error',
         {
@@ -49,47 +56,14 @@ module.exports = !SVELTE
           },
         },
       ],
-      'svelte/html-self-closing': [
-        'error',
-        'all',
-      ],
-      'svelte/indent': [
-        'error',
-        {
-          indent: 2,
-          ignoredNodes: [],
-          switchCase: 1,
-          alignAttributesVertically: false,
-        },
-      ],
-      'svelte/sort-attributes': [
-        'error',
-        {
-          order: [
-            'this',
-            'bind:this',
-            'id',
-            'name',
-            'slot',
-            { match: '/^--/u', sort: 'alphabetical' },
-            ['style', '/^style:/u'],
-            'class',
-            { match: '/^class:/u', sort: 'alphabetical' },
-            {
-              match: ['!/:/u', '!/^(?:this|id|name|style|class)$/u', '!/^--/u'],
-              sort: 'alphabetical',
-            },
-            ['/^bind:/u', '!bind:this', '/^on:/u'],
-            { match: '/^use:/u', sort: 'alphabetical' },
-            { match: '/^transition:/u', sort: 'alphabetical' },
-            { match: '/^in:/u', sort: 'alphabetical' },
-            { match: '/^out:/u', sort: 'alphabetical' },
-            { match: '/^animate:/u', sort: 'alphabetical' },
-            { match: '/^let:/u', sort: 'alphabetical' },
-          ],
-        },
-      ],
+      'svelte/html-self-closing': 'error',
+      'svelte/indent': 'error',
+      'svelte/mustache-spacing': 'error',
+      'svelte/no-spaces-around-equal-signs-in-attribute': 'error',
       'svelte/prefer-class-directive': 'error',
       'svelte/prefer-style-directive': 'error',
+      'svelte/sort-attributes': 'error',
+      'svelte/spaced-html-comment': 'error',
+      'svelte/no-trailing-spaces': 'error',
     },
   })
